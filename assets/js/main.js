65cdeb1c6c4fed1b652385103007997f3f2c568d
EnkiBrant <enki.brant@gmail.com>
2021-09-10 18:06:16 +0200

    add css

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// const btn = document.querySelector("#submit");
+// const messageAlert = document.querySelector("#alert p");
+
+// messageAlert.textContent = "";
+// // console.log(btn);
+
+// const alertMe = () => console.log("prout");
+
+// btn.addEventListener("onclick",alertMe)
